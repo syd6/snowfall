e1d173af175d78b50c60d741cf5a559c9c7d51d4
syd6 <syd6@cornell.edu>
2016-11-23 16:03:19 -0500

    added snowfall function

--- js/weather.js
+++ js/weather.js
@@ -73,10 +73,8 @@ $(document).ready(function(){
 		var hours = d.getHours();
 		var mins = d.getMinutes();
 		var secs = d.getSeconds();
-		//FROM 12AM-6AM dark 
-		//total mins elapsed: 360 mins
-		//20%-35% (increase in 15% over 360 mins)
-		//brighten at a rate of 0.04% per minute
+		//FROM 12AM-6AM dark. total mins elapsed: 360 
+		//20%-35% (increase in 15% over 360 mins). Brighten at rate of 0.04% per min
 		//min1 = 20% + 0.04%, min2 = 20+(0.04%)2, min3 = 20+(0.04%)3, etc
 		if (hours >= 0 && hours <6){
 			//brightness = 20% + 0.04 (total time elapsed so far)
@@ -92,13 +90,9 @@ $(document).ready(function(){
 		}
 		
 		if (hours >= 15 && hours <18){
-			//total minutes elapsed: 180 
-			//85% - 40% for brightness
-			//decrease in 45% over 180 minutes
-			//darken at a rate of 0.25% per minute
-			//saturation goes from 70-50
-			//decrease in 20% over 180 minutes
-			//desaturate at a rate of 0.11% perminute
+			//total minutes elapsed: 180.
+			//85% - 40% for brightness. decrease in 45% over 180 minutes. darken at a rate of 0.25% per minute
+			//saturation goes from 70-50. decrease in 20% over 180 minutes. desaturate at a rate of 0.11% perminute
 			var brightness = 85-0.25*(((hours-15)*60)+mins);
 			//have to subtract by 15 bc ur starting at 0 hr elapsed for this color cycle 
 			var desaturation = 70-0.11*(((hours-15)*60)+mins);
@@ -161,4 +155,102 @@ setInterval(displayTime, 1000);
 	
 /***********END DISPLAY TIME**********
 *************************************/
+
+
+
+
+/*idea: force directed diagram somewhere?? (Snowflakes: maybe don't need to use aftereffects)
+http://christophermanning.org/projects/voronoi-diagram-with-force-directed-nodes-and-delaunay-links
+*/
+
+
+
+//code: http://thecodeplayer.com/walkthrough/html5-canvas-snow-effect?s=rlt
+/******************UPDATE THIS TO REFLECT OPEN WEATHER MAP CONDITIONS ************/
+
+
+window.onload = function(){
+	var canvas = document.getElementById("canvas1");
+	//returns an object that provides methods and properties for drawing on the canvas
+	var drawcontxt = canvas.getContext("2d");
+
+
+	//specify canvas dimensions
+	var W = window.innerWidth/1.3;
+
+	var H = window.innerHeight;
+	canvas.width = W;
+	canvas.height = H;
+
+	//snowflake particles
+	var maxparts = 20; //max number of particles on the screen at any time
+	var particles = []; //array for holding particles
+	for (var i=0;i<maxparts;i++){
+		//TODO: search push function up!!! and see what it does specifically
+		particles.push({
+			//fields of a particle: calling particle.x will select x property 
+			x: Math.random()*W, //width of the snow particle
+			y: Math.random()*H, //height of the snow particle
+			r: Math.random()*4+1, //radius of the snow particle
+			d: Math.random()*maxparts //density of snow particles: used to calculate how they fall
+		})
+	}
+
+	//DRAWING FUNCTION 
+	function draw(){
+		//Clears the specified pixels within a given rectangle
+		drawcontxt.clearRect(0, 0, W, H);
+		//specifies the color used to draw 
+		drawcontxt.fillStyle = "rgba(255,255,255,0.9)";
+		//begins path or resets current path
+		drawcontxt.beginPath();
+		//for loop draws the actual circles
+		for (var i=0;i<maxparts;i++){
+			//select individual particle
+			var p = particles[i];
+			/*moves path to specified point (random x y coordinates 
+			generated using particles.push) without creating a line*/
+			drawcontxt.moveTo(p.x, p.y);
+			//draw the circle that represents each individual flake
+			//true means draw counterclockwise. this argument is optional
+			drawcontxt.arc(p.x, p.y, p.r, 0, Math.PI*2, true);
+		}
+		//fills current drawing
+		drawcontxt.fill();
+		//call the function that makes the particles move 
+		update();
+	}
+	var angle = 0;
+	function update(){
+		angle += 0.01;
+		for (var i=0;i<maxparts;i++){
+			//select individual particle
+			var p = particles[i];
+			/*have to add one to cosine to prevent negative values, 
+			which cause flakes to move upwards*/
+			//density comes into play: makes downward movement for every flake different
+			//add in radius: makes it more random
+			p.y += Math.cos(angle+p.d) + 1+ p.r/2;
+			p.x += Math.sin(angle)*2;
+			//sends flake back to top when it exits screen
+			//lets flakes come in from l and r
+
+			//if the location of the flake is off the screen on bottom or sides
+			if (p.x> W+5 || p.x < -5 || p.y >H){
+				if (i%3>0){//2/3 of flakes
+					particles[i] = {x: Math.random()*W, y: -10, r: p.r, d: p.d};
+				} 
+				else{
+					//if flake exits from right, enter from left
+					if (Math.sin(angle)>0) particles[i] = {x: -5, y: Math.random()*H, r: p.r, d: p.d};
+					//else if flake exist from left, enter from right
+					else particles[i] ={x: W+5, y: Math.random()*H, r: p.r, d: p.d};
+				}
+			}
+		}
+	}
+	//loop
+	setInterval(draw, 50);
+}
+
 
